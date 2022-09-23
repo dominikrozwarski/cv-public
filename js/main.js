92b3c83baaa7496ce718e7500ae7adc3d1ed0449
therum <61469592+dominikrozwarski@users.noreply.github.com>
2022-09-23 17:40:25 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -2,26 +2,59 @@ const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
 
-const navOne = document.querySelector('.nav__desk');
-const navTwo = document.querySelector('.nav__desk2');
-const navThree = document.querySelector('.nav__desk3');
+const navOne = document.querySelectorAll('.nav__desk');
 
+
+
+
+
+
+
+
+//hidding nav
 const clear = () => {
 	navBtn.classList.toggle('is-active');
 	navMobile.classList.toggle('nav-mobile--active');
 };
 
-navOne.addEventListener('click', clear);
-navTwo.addEventListener('click', clear);
-navThree.addEventListener('click', clear);
+navOne.forEach(nav=>{
+	nav.addEventListener('click', clear)
+})
+
 
 const handleNav = () => {
+	handleNavItemsAnimations()
 	navBtn.classList.toggle('is-active');
 	navMobile.classList.toggle('nav-mobile--active');
 };
 
 navBtn.addEventListener('click', handleNav);
 
+
+//animation delay time
+
+const handleNavItemsAnimations =()=>{
+	let delayTime =0;
+	navOne.forEach(nav =>{
+		nav.classList.toggle('nav-items-animation')
+		nav.style.animationDelay = '.' + delayTime + 's';
+		delayTime++
+	})
+	
+}
+
+
+
+//typing
+let typed = new Typed(".auto-type", {
+	strings:[" a frontend developer", "a designer", "in need for work :)", "jobless..."],
+	typeSpeed:50,
+	backSpeed:50,
+	loop:true
+})
+
+
+//footer year
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;

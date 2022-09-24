f580896db914e8c314ecba059803360da9da9aae
therum <61469592+dominikrozwarski@users.noreply.github.com>
2022-09-24 12:30:04 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
-const footerYear = document.querySelector('.footer__year');
-
 const navOne = document.querySelectorAll('.nav__desk');
-
-
-
-
-
-
-
 
 //hidding nav
 const clear = () => {
@@ -17,46 +8,33 @@ const clear = () => {
 	navMobile.classList.toggle('nav-mobile--active');
 };
 
-navOne.forEach(nav=>{
-	nav.addEventListener('click', clear)
-})
-
+navOne.forEach((nav) => {
+	nav.addEventListener('click', clear);
+});
 
 const handleNav = () => {
-	handleNavItemsAnimations()
+	handleNavItemsAnimations();
 	navBtn.classList.toggle('is-active');
 	navMobile.classList.toggle('nav-mobile--active');
 };
 
 navBtn.addEventListener('click', handleNav);
 
-
 //animation delay time
 
-const handleNavItemsAnimations =()=>{
-	let delayTime =0;
-	navOne.forEach(nav =>{
-		nav.classList.toggle('nav-items-animation')
+const handleNavItemsAnimations = () => {
+	let delayTime = 0;
+	navOne.forEach((nav) => {
+		nav.classList.toggle('nav-items-animation');
 		nav.style.animationDelay = '.' + delayTime + 's';
-		delayTime++
-	})
-	
-}
-
-
+		delayTime++;
+	});
+};
 
 //typing
-let typed = new Typed(".auto-type", {
-	strings:[" a frontend developer", "a designer", "your new team member "],
-	typeSpeed:100,
-	backSpeed:50,
-	loop:true
-})
-
-
-//footer year
-const handleCurrentYear = () => {
-	const year = new Date().getFullYear();
-	footerYear.innerText = year;
-};
-handleCurrentYear();
+let typed = new Typed('.auto-type', {
+	strings: [' a frontend developer', 'a designer', 'your new team member '],
+	typeSpeed: 100,
+	backSpeed: 50,
+	loop: true,
+});
